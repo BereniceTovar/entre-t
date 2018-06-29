738d6ee479b30a941e15ebe9e32a9ca508e59f77
latilanoh@gmail.com <latilanoh@gmail.com>
2018-06-29 00:15:43 -0500

    se retoma trabajo para la autentificacion con gmail, se modifica partes de la extructura en user para en el siguiente alcance imprimir los datos del usuario y se trabaja con la funcionalidad para realizar tanto login como logout

--- js/index.js
+++ js/index.js
@@ -1,18 +1,7 @@
-var config = {
-    apiKey: "AIzaSyDj_C75Jq1mQpeBNjSGEpJPLhjSIm_-IFE",
-    authDomain: "entre-t.firebaseapp.com",
-    databaseURL: "https://entre-t.firebaseio.com",
-    projectId: "entre-t",
-    storageBucket: "entre-t.appspot.com",
-    messagingSenderId: "1017925579785"
-};
-
-firebase.initializeApp(config);
-
-/* Variable global con la información del usuario */
+//  Variable global con la información del usuario 
 var userInfo = {};
 
-/* Función para guardar la info de los usuarios */
+// Función para guardar la info de los usuarios 
 function saveUser(user) {
     var userInfo = {
         uid: user.uid,
@@ -22,11 +11,81 @@ function saveUser(user) {
 
    firebase.database().ref("users/" + user.uid).set(userInfo);
 };
-    
+
+$('#btn-signup').click(function(){
+    console.log('boton clickeado signup');
+
+    $('#modal-SignUp').modal('show');
+
+    //Falta corregir esta parte para que se guarde la info correctamente de
+    //nvos usuarios en firebase
+    var email = $('#input-email').val();
+    var password = $('#input-password').val();
+    window.location ="user.html";
+    console.log(email,password);
+
+    firebase.auth().signInWithEmailAndPassword(email,password)
+    .catch( function(error) {
+        //alert(error.message);
+    });
+});
+
+$("#btn-login").click(function(){
+    // console.log('login')
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider).then(function(result){
+        saveUser(result.user);
+        $('.photo-user').append('<img class="photo-login" href="user.html" alt="user" src="' + result.user.photoURL + '"/>');
+        $('.user-name').append(result.user.displayName);
+        window.location ="user.html";
+        //console.log(result.user.displayName);
+    })
+    .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+
+});
+
+/*
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+    window.location ="user.html";
+    } else {
+    console.log('desloggeado');
+    }
+});*/
+
+
+
+
+
+/* Variable global con la información del usuario 
+var userInfo = {}; */
+
+/* Función para guardar la info de los usuarios 
+function saveUser(user) {
+    var userInfo = {
+        uid: user.uid,
+        name: user.displayName,
+        photo: user.photoURL,
+    };
+
+   firebase.database().ref("users/" + user.uid).set(userInfo);
+}; */
+
+/*
 var provider = new firebase.auth.GoogleAuthProvider();
 
 $("btn-login").click(function(){
-    //console.log('login');
+    console.log('login');
     firebase.auth().signInWithPopup(provider).then(function(result){
         saveUser(result.user);
         //$('#photo-user').append("<img src='" + result.user.photoURL + "' />");
@@ -48,7 +107,7 @@ $("btn-login").click(function(){
 });
 
 document.getElementById("btn-logout").addEventListener('click',function (){
-    //console.log('logout');
+    console.log('logout');
     firebase.auth().signOut();
 });
 
@@ -58,17 +117,19 @@ firebase.auth().onAuthStateChanged(function(user) {
     } else {
     console.log('desloggeado');
     }
-});
+});*/
 
+/*
 $(document).ready(function () {
 
-    // // ******************FUNCION PARA SPLASH******************//
+    // ******************FUNCION PARA SPLASH******************
     // setTimeout(function () {
     //     $('#splash').fadeOut(500);
     // }, 1000);
-});    
+});   */
 
+/*
 document.getElementById("btn-signup").addEventListener('click',function(){
     //console.log("sign up");
     $('#modal-SignUp').modal('show');
-});
+});*/
